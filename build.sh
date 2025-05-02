e084035528686d8442ae9d1fede6afcfa42af1df
Bekzat Baubek <39556077+bekzatbaubek@users.noreply.github.com>
2025-05-02 19:30:08 +0800

    cleanup

--- build.sh
+++ build.sh
@@ -2,30 +2,25 @@
 
 mkdir -p build
 
-# glslangValidator -V shaders/heart.vert -o shaders/heart.vert.spv
-# glslangValidator -V shaders/heart.frag -o shaders/heart.frag.spv
-
-# clang++ \
-#     --std=c++20 \
-#     -g \
-#     vulkan-heart.cpp \
-#     -o ./build/vulkan-heart \
-#     -Ivendor/ \
-#     -lglfw \
-#     -lvulkan
+glslangValidator -V shaders/heart.vert -o shaders/heart.vert.spv
+glslangValidator -V shaders/heart.frag -o shaders/heart.frag.spv
 
 clang++ \
-    --std=c++20 \
+    --std=c++17 \
     -g \
-    vkh.cpp \
+    vkh_game.cpp \
     -shared \
     -o ./build/lib.so
 
 clang++ \
-    --std=c++20 \
+    --std=c++17 \
     -g \
+    vkh_renderer.cpp \
     vkh_platform.cpp \
     -o ./build/vkh_platform \
     -Ivendor/ \
     -lglfw \
     -lvulkan
+
+# Curse upon rpath
+install_name_tool -add_rpath /usr/local/lib ./build/vkh_platform
--- vkh.cpp
+++
@@ -1,9 +0,0 @@
-#include <cstdio>
-
-#include "vkh.hpp"
-
-void gameUpdateAndRender(GameState* state){
-    printf("Something else as well\n");
-    printf("Hahah updated! From game code: %d\n", state->counter);
-    state->counter++;
-}
